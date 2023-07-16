8eaebb08b13f0c96961a308b8d9a049327831569
iwakazuk <i.kazu861@gmail.com>
2023-07-16 23:28:05 +0900

    ホーム画面にスクロールビュー追加

--- app/src/main/java/com/purelab/fragment/blankfragment/HomeFragment1.kt
+++ app/src/main/java/com/purelab/fragment/blankfragment/HomeFragment1.kt
@@ -7,107 +7,68 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.card.MaterialCardView
 import com.purelab.R
+import com.purelab.adapters.CategoryListAdapter
+import com.purelab.adapters.ItemCardListAdapter
 import com.purelab.databinding.FragmentHome1Binding
+import com.purelab.models.Item
+import com.purelab.models.mockItem
 
 class HomeFragment1 : BaseDataBindingFragment<FragmentHome1Binding>() {
 
     override fun getLayoutRes(): Int = R.layout.fragment_home1
-    private var currentPosition = 0
-    private val itemList = listOf(
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    )
+    private var itemList: List<Item> = emptyList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-        val homeView: View =
-            inflater.inflate(R.layout.fragment_home1, container, false) ?: return null
-        setupMotion(homeView)
-        return homeView
-    }
 
-    private fun setupMotion(homeView: View) {
-        val motionLayout: MotionLayout = homeView.findViewById(R.id.motionLayout)
-        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionTrigger(
-                motionLayout: MotionLayout?,
-                triggerId: Int,
-                positive: Boolean,
-                progress: Float
-            ) {
-            }
+        val binding = dataBinding!!
+        itemList = listOf(mockItem(), mockItem(), mockItem(), mockItem(), mockItem(), mockItem())
 
-            override fun onTransitionStarted(
-                motionLayout: MotionLayout?,
-                startedId: Int,
-                endId: Int
-            ) {
-            }
+        // アダプターをセット
+        val bookListRecyclerView: RecyclerView = binding.itemCardListView
+        val linearLayoutManager = LinearLayoutManager(requireActivity())
+        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        val adapter = ItemCardListAdapter(itemList)
 
-            override fun onTransitionChange(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int,
-                progress: Float
-            ) {
-            }
+        bookListRecyclerView.layoutManager = linearLayoutManager
+        bookListRecyclerView.adapter = adapter
+        bookListRecyclerView.addItemDecoration(
+            DividerItemDecoration(
+                requireActivity(),
+                linearLayoutManager.orientation
+            )
+        )
 
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-                if (motionLayout?.progress == 0.toFloat()) {
-                    return
-                }
-                when (currentId) {
-                    R.id.move_left_to_right -> {
-                        if (currentPosition > 0) {
-                            currentPosition--
-                        } else {
-                            currentPosition = itemList.lastIndex
-                        }
-                        motionLayout?.progress = 0F
-                        updateView(homeView)
-                    }
+        // CellItem要素クリックイベント
+        adapter.setOnItemClickListener(
+            // BookListRecyclerViewAdapterで定義した抽象メソッドを実装
+            // 再利用をしないため object式でインターフェースを実装
 
-                    R.id.move_right_to_left -> {
-                        if (currentPosition < itemList.lastIndex) {
-                            currentPosition++
-                        } else {
-                            currentPosition = 0
-                        }
-                        motionLayout?.progress = 0F
-                        updateView(homeView)
-                    }
+            object : ItemCardListAdapter.OnItemClickListener {
+                override fun onItemClick(itemId: String?) {
+                    setFragmentResult(
+                        "request_key",
+                        bundleOf("itemId" to itemId)
+                    )
+                    findNavController().navigate(
+                        R.id.action_dashboardFragment2_to_dashboardFragment3
+                    )
                 }
             }
-        })
-        updateView(homeView)
-    }
-
-    private fun updateView(homeView: View) {
-        val centerTextView: MaterialCardView = homeView.findViewById(R.id.centerView)
-        val rightTextView: MaterialCardView = homeView.findViewById(R.id.rightView)
-        val leftTextView: MaterialCardView = homeView.findViewById(R.id.leftView)
-//
-//        centerTextView.text = "Item\n${itemList[currentPosition]}"
-//
-//        rightTextView.text = if (currentPosition == itemList.lastIndex) {
-//            "Item\n${itemList.first()}"
-//        } else {
-//            "Item\n${itemList[currentPosition + 1]}"
-//        }
-//
-//        leftTextView.text = if (currentPosition == 0) {
-//            "Item\n${itemList.last()}"
-//        } else {
-//            "Item\n${itemList[currentPosition - 1]}"
-//        }
-
+        )
+        return binding.root
     }
 }
