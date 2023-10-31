4fd13fcbc6e888a89cc1000690fe3b38b72c74a2
iwakazuk <i.kazu861@gmail.com>
2023-10-31 22:51:31 +0900

    管理者画面追加

--- app/src/main/java/com/purelab/view/admin/AdminFragment.kt
+++ app/src/main/java/com/purelab/view/admin/AdminFragment.kt
@@ -1,18 +1,23 @@
 package com.purelab.view.admin
 
-import android.graphics.Rect
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.text.InputType
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import com.purelab.R
+import com.purelab.databinding.AdminInputSectionBinding
 import com.purelab.databinding.FragmentAdminBinding
 import com.purelab.utils.CustomSnackbar
-import com.purelab.utils.hideKeyboard
 import com.purelab.view.BaseDataBindingFragment
 
 class AdminFragment : BaseDataBindingFragment<FragmentAdminBinding>() {
@@ -28,55 +33,165 @@ class AdminFragment : BaseDataBindingFragment<FragmentAdminBinding>() {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentAdminBinding.inflate(inflater, container, false)
 
-        // ブランドのピッカーを設定
+        // 全データのロード
+        vm.loadItems()
         vm.loadBrands()
-        vm.brands.observe(viewLifecycleOwner) { brandList ->
-            val adapter = ArrayAdapter(requireContext(), R.layout.custom_spinner_item, brandList)
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            binding.adminBrand.adapter = adapter
-        }
-
-        // カテゴリのピッカーを設定
         vm.loadCategories()
-        vm.categories.observe(viewLifecycleOwner) { categoryList ->
-            val adapter = ArrayAdapter(requireContext(), R.layout.custom_spinner_item, categoryList)
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            binding.adminCategory.adapter = adapter
-        }
-
-        // 成分のピッカーを設定
         vm.loadIngredients()
-        vm.selectedIngredients.observe(viewLifecycleOwner) { ingredients ->
-            val text = if (ingredients.isEmpty()) {
-                "成分を選択"
-            } else {
-                ingredients.joinToString(separator = "\n")
-            }
-            binding.adminIngredientsLabel.text = text
-        }
+
+        // キーボードの設定
+        settingKeyBord(binding.adminName)
+
+        // 各セクションの描画
+        setItemSection()
+        setupInputSection(binding.brandInputSection, "ブランド")
+        setupInputSection(binding.categoryInputSection, "カテゴリ")
+        setupInputSection(binding.ingredientInputSection, "成分")
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setHideKeyBoard(binding.root)
 
+        // 各種ボタンの設定
+        setIngredientButton()
+        setupItemSaveButton()
+        setupBrandSaveButton(binding.brandInputSection)
+        setupCategorySaveButton(binding.categoryInputSection)
+        setupIngredientSaveButton(binding.ingredientInputSection)
+    }
+
+    /** 商品を追加セクションの描画設定 */
+    private fun setItemSection() {
+        // ブランドのピッカーを設定
+        vm.brands.observe(viewLifecycleOwner) { brandMap ->
+            val brandList = brandMap.map { it.second }
+            val adapter = ArrayAdapter(requireContext(), R.layout.custom_spinner_item, brandList)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.adminBrand.adapter = adapter
+        }
+
+        // カテゴリのピッカーを設定
+        vm.categories.observe(viewLifecycleOwner) { categoryMap ->
+            val categoryList = categoryMap.map { it.second }
+            val adapter = ArrayAdapter(requireContext(), R.layout.custom_spinner_item, categoryList)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.adminCategory.adapter = adapter
+        }
+
+        // 成分のピッカーを設定
+        vm.selectedIngredients.observe(viewLifecycleOwner) { ingredients ->
+            val text = if (ingredients.isEmpty()) {
+                "成分を選択"
+            } else {
+                ingredients.joinToString(separator = "\n")
+            }
+            binding.adminIngredients.text = text
+        }
+    }
+
+    /** 各セクションのレイアウトを描画 */
+    private fun setupInputSection(
+        section: AdminInputSectionBinding,
+        label: String
+    ) {
+        section.titleText.text = label + "を追加"
+        section.labelText.text = label + "名"
+        section.inputField.hint = label + "名を入力"
+        section.saveButton.text = "保存"
+        settingKeyBord(section.inputField)
+    }
+
+    /** 商品追加セクションの保存ボタンのクリックリスナーを設定 */
+    private fun setupItemSaveButton() {
         binding.saveButton.setOnClickListener { v ->
+            val name: String = binding.adminName.text.toString()
+            val brandingID = binding.adminBrand.textAlignment.toString()
+            val categoryID = binding.adminCategory.textAlignment.toString()
+            val ingredientIDs = listOf(vm.selectedIngredients.value)
+
+            if (name.isEmpty()) {
+                CustomSnackbar.showSnackBar(v, "商品名を入力してください")
+                return@setOnClickListener
+            } else if (brandingID == null) {
+                CustomSnackbar.showSnackBar(v, "ブランド名を入力してください")
+                return@setOnClickListener
+            } else if (categoryID == null) {
+                CustomSnackbar.showSnackBar(v, "カテゴリを選択してください")
+                return@setOnClickListener
+            } else if (ingredientIDs.isEmpty()) {
+                CustomSnackbar.showSnackBar(v, "成分を選択してください")
+                return@setOnClickListener
+            }
+
             val product = hashMapOf(
-                "name" to binding.adminName.text,
-                "brandID" to binding.adminBrand.textAlignment,
-                "categoryID" to binding.adminCategory.textAlignment,
-                "ingredientIDs" to listOf(binding.adminIngredients)
+                "name" to name,
+                "brandID" to brandingID,
+                "categoryID" to categoryID,
+                "ingredientIDs" to ingredientIDs
             )
             vm.saveItem(product)
 
             // 保存完了のsnackbarを表示
-            CustomSnackbar.showSnackBar(view, "商品を保存しました")
+            CustomSnackbar.showSnackBar(v, "商品を保存しました")
         }
+    }
 
+    /** ブランド追加セクションの保存ボタンのクリックリスナーを設定 */
+    private fun setupBrandSaveButton(binding: AdminInputSectionBinding) {
+        binding.saveButton.setOnClickListener { v ->
+            val name: String = binding.inputField.text.toString()
+            if (name.isEmpty()) {
+                CustomSnackbar.showSnackBar(v, "ブランド名を入力してください")
+                return@setOnClickListener
+            }
+            val product = hashMapOf("name" to name)
+            vm.saveBrand(product)
+
+            // 保存完了のsnackbarを表示
+            CustomSnackbar.showSnackBar(v, "ブランド名を保存しました")
+        }
+    }
+
+
+    /** ブランド追加セクションの保存ボタンのクリックリスナーを設定 */
+    private fun setupCategorySaveButton(binding: AdminInputSectionBinding) {
+        binding.saveButton.setOnClickListener { v ->
+            val name: String = binding.inputField.text.toString()
+            if (name.isEmpty()) {
+                CustomSnackbar.showSnackBar(v, "カテゴリ名を入力してください")
+                return@setOnClickListener
+            }
+            val product = hashMapOf("name" to name)
+            vm.saveCategory(product)
+
+            // 保存完了のsnackbarを表示
+            CustomSnackbar.showSnackBar(v, "カテゴリ名を保存しました")
+        }
+    }
+
+
+    /** ブランド追加セクションの保存ボタンのクリックリスナーを設定 */
+    private fun setupIngredientSaveButton(binding: AdminInputSectionBinding) {
+        binding.saveButton.setOnClickListener { v ->
+            val name: String = binding.inputField.text.toString()
+            if (name.isEmpty()) {
+                CustomSnackbar.showSnackBar(v, "成分名を入力してください")
+                return@setOnClickListener
+            }
+            val product = hashMapOf("name" to name)
+            vm.saveIngredient(product)
+
+            // 保存完了のsnackbarを表示
+            CustomSnackbar.showSnackBar(v, "成分名を保存しました")
+        }
+    }
+
+    /** 成分選択ボタンの設定 */
+    private fun setIngredientButton() {
         binding.adminIngredients.setOnClickListener { v ->
-            val ingredients = vm.ingredients.value?.toTypedArray() ?: arrayOf()
+            val ingredients = vm.ingredients.value?.map { it.second }?.toTypedArray() ?: arrayOf()
             val checkedItems = BooleanArray(ingredients.size) // 初期状態ではすべての項目を未選択に設定
 
             AlertDialog.Builder(requireContext())
@@ -87,37 +202,39 @@ class AdminFragment : BaseDataBindingFragment<FragmentAdminBinding>() {
                 .setPositiveButton("OK") { _, _ ->
                     val selectedIngredients =
                         ingredients.filterIndexed { index, _ -> checkedItems[index] }
-                    vm.selectedIngredients.value = selectedIngredients
+                    val selectedIngredientsPair = vm.ingredients.value?.mapNotNull {
+                        if (selectedIngredients.contains(it.second)) {
+                            it
+                        } else {
+                            null
+                        }
+                    }
+
+                    vm.selectedIngredients.value = selectedIngredientsPair
                 }
                 .setNegativeButton("Cancel", null)
                 .show()
         }
     }
 
-    private fun setHideKeyBoard(view: View) {
-        // タッチリスナーを設定
-        view.setOnTouchListener { v, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    if (!isKeyboardShown(view)) {
-                        requireContext().hideKeyboard(view)
-                    }
-                }
-
-                MotionEvent.ACTION_UP -> {
-                    v.performClick()
-                }
+    private fun settingKeyBord(editText: EditText) {
+        editText.inputType = InputType.TYPE_CLASS_TEXT // Add this line
+        editText.setOnKeyListener { _, keyCode, event ->  // Add this block
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == MotionEvent.ACTION_DOWN) {
+                hideKeyboardFrom(requireContext(), editText)
+                return@setOnKeyListener true
             }
-            true
+            false
         }
     }
 
-    private fun isKeyboardShown(rootView: View): Boolean {
-        val softKeyboardHeight = 100
-        val r = Rect()
-        rootView.getWindowVisibleDisplayFrame(r)
-        val screenHeight = rootView.height
-        val keypadHeight = screenHeight - r.bottom
-        return keypadHeight > softKeyboardHeight
+    private fun hideKeyboardFrom(context: Context, view: View) {
+        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        hideKeyboardFrom(requireContext(), binding.root)
     }
 }
--- app/src/main/java/com/purelab/view/admin/AdminViewModel.kt
+++ app/src/main/java/com/purelab/view/admin/AdminViewModel.kt
@@ -5,46 +5,122 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.FirebaseFirestore
-import com.purelab.models.User
-import io.realm.Realm
 
 class AdminViewModel(
     application: Application
 ) : AndroidViewModel(application) {
-    val db = FirebaseFirestore.getInstance()
+    private val db = FirebaseFirestore.getInstance()
 
     // LiveData for brands and categories
-    val brands = MutableLiveData<List<String>>()
-    val categories = MutableLiveData<List<String>>()
-    val ingredients = MutableLiveData<List<String>>()
-    val selectedIngredients = MutableLiveData<List<String>>()
+    private val itemIds = MutableLiveData<List<String>>()
+    val brands = MutableLiveData<List<Pair<String, String>>>()
+    val categories = MutableLiveData<List<Pair<String, String>>>()
+    val ingredients = MutableLiveData<List<Pair<String, String>>>()
+    val selectedIngredients = MutableLiveData<List<Pair<String, String>>>()
+
+    fun loadItems() {
+        db.collection("items").get().addOnSuccessListener { querySnapshot ->
+            val itemIdList = querySnapshot.documents.mapNotNull { it.id }
+            itemIds.value = itemIdList
+        }
+    }
 
     fun loadBrands() {
         db.collection("brands").get().addOnSuccessListener { querySnapshot ->
-            val brandList = querySnapshot.documents.mapNotNull { it.getString("name") }
+            val brandList = querySnapshot.documents.mapNotNull {
+                val name = it.getString("name")
+                if (name != null) Pair(it.id, name) else null
+            }
             brands.value = brandList
         }
     }
 
     fun loadCategories() {
         db.collection("categories").get().addOnSuccessListener { querySnapshot ->
-            val categoryList = querySnapshot.documents.mapNotNull { it.getString("name") }
+            val categoryList = querySnapshot.documents.mapNotNull {
+                val name = it.getString("name")
+                if (name != null) Pair(it.id, name) else null
+            }
             categories.value = categoryList
         }
     }
 
     fun loadIngredients() {
         db.collection("ingredients").get().addOnSuccessListener { querySnapshot ->
-            val categoryList = querySnapshot.documents.mapNotNull { it.getString("name") }
+            val categoryList = querySnapshot.documents.mapNotNull {
+                val name = it.getString("name")
+                if (name != null) Pair(it.id, name) else null
+            }
             categories.value = categoryList
         }
     }
 
     fun saveItem(item: Map<String, Any>) {
+        val itemId = itemIds.value?.last()?.toInt()?.plus(1)?.toString()
+        if (itemId == null) {
+            Log.w("FirestoreApp", "Error retrieving itemId")
+            return
+        }
+
         db.collection("items")
-            .add(item)
-            .addOnSuccessListener { documentReference ->
-                Log.d("FirestoreApp", "DocumentSnapshot added with ID: ${documentReference.id}")
+            .document(itemId)
+            .set(item)
+            .addOnSuccessListener {
+                Log.d("FirestoreApp", "DocumentSnapshot added with ID: $itemId")
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreApp", "Error adding document", e)
+            }
+    }
+
+    fun saveBrand(brand: Map<String, String>) {
+        val brandId = brands.value?.map { it.first }?.last()?.toInt()?.plus(1)?.toString()
+        if (brandId == null) {
+            Log.w("FirestoreApp", "Error retrieving brandId")
+            return
+        }
+
+        db.collection("brands")
+            .document(brandId)
+            .set(brand)
+            .addOnSuccessListener {
+                Log.d("FirestoreApp", "DocumentSnapshot added with ID: $brandId")
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreApp", "Error adding document", e)
+            }
+    }
+
+    fun saveCategory(category: Map<String, String>) {
+        val categoryId = categories.value?.map { it.first }?.last()?.toInt()?.plus(1)?.toString()
+        if (categoryId == null) {
+            Log.w("FirestoreApp", "Error retrieving categoryId")
+            return
+        }
+
+        db.collection("brands")
+            .document(categoryId)
+            .set(category)
+            .addOnSuccessListener {
+                Log.d("FirestoreApp", "DocumentSnapshot added with ID: $categoryId")
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreApp", "Error adding document", e)
+            }
+    }
+
+    fun saveIngredient(ingredient: Map<String, String>) {
+        val ingredientId = ingredients.value?.map { it.first }?.last()?.toInt()?.plus(1)?.toString()
+        if (ingredientId == null) {
+            Log.w("FirestoreApp", "Error retrieving ingredientId")
+            return
+        }
+
+        db.collection("ingredients")
+            .document(ingredientId)
+            .set(ingredient)
+            .addOnSuccessListener {
+                Log.d("FirestoreApp", "DocumentSnapshot added with ID: $ingredientId")
             }
             .addOnFailureListener { e ->
                 Log.w("FirestoreApp", "Error adding document", e)
